b1f10a7deec890bffdb42e60e8de71030c075e17
Evelyn Hernandez <hernandeze791@gmail.com>
2020-09-11 17:08:53 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -20,7 +20,7 @@ def play(songs)
 end
 
 def list(songs)
-  songs.each_with_index do {|song, index|
-    puts "#{index+1}. #{song}"
-  }
+songs.each_with_index {|song, index|
+  puts "#{index+1}. #{song}"
+}
 end
